9ba05e2a7c180a2ef6ba405a469e7317e021b67d
Chenpeng Du <32744084+cpdu@users.noreply.github.com>
2020-02-28 14:12:05 +0800

    Update save_rnnt_alignments.sh

--- save_rnnt_alignments.sh
+++ save_rnnt_alignments.sh
@@ -16,8 +16,8 @@ ngpu=1         # number of gpus ("0" uses cpu, otherwise use gpu)
 batchsize=24
 
 expdir=exp/train_nodup_pytorch_train_transducer
-model=${expdir}/results/snapshot.ep.18
-json=dump/train_nodup/deltafalse/data_phn_dev.json
+model=${expdir}/results/snapshot.ep.xx
+json=dump/train_nodup/deltafalse/data.json
 
 npydir=${expdir}/calculate_alignments/`basename ${model}`/`basename ${json} .json`
 if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
